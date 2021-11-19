2cf55ccbcb0e4b860a3bda4122877eca8605fd90
Jordan Pitcairn <jpitcairn@me.com>
2021-11-19 12:27:59 +0000

    User Table added

--- app/models.py
+++ app/models.py
@@ -1,12 +1,23 @@
 from datetime import datetime
 from app import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(120))
+    virtual_machines = db.relationship('VirtualMachine', backref='creator', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
 class VirtualMachine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     vm_id = db.Column(db.Integer, index=True)
     node = db.Column(db.String(120))
     name = db.Column(db.String(120), index=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    creator = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<VirtualMachine {}>'.format(self.name)
