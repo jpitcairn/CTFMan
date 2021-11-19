c5f2f7b4ae2a0a790b31a5dc5cfc8c83b3d9a705
Jordan Pitcairn <jpitcairn@me.com>
2021-11-19 18:42:20 +0000

    User System added

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import SubmitField, StringField, PasswordField
+from wtforms import validators
+from wtforms.validators import DataRequired, ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address')
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,59 @@
-from flask import render_template, flash, redirect, url_for
-from app import app, forms
+from flask import render_template, flash, redirect, url_for, request
+from app import app, forms, db
 from app.hypervisor import get_virtual_machines, start_virtual_machine, shutdown_virtual_machine, reset_virtual_machine
-from app.forms import EmptyForm
+from app.forms import EmptyForm, LoginForm, RegisterForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
+# Index page routing
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
-    user = {'username':'Jordan'}
     form = EmptyForm()
     virtual_machines = get_virtual_machines()
-    return render_template('index.html', title='Home', slug='Control VMs', virtual_machines=virtual_machines, form=form, user=user)
+    return render_template('index.html', title='Home', slug='Control VMs', virtual_machines=virtual_machines, form=form)
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
+
+# Logout
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+# Register
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect (url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registered Successfully')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 # Start Virtual Machine
 @app.route('/vm/start/<vmid>', methods=['POST'])
@@ -43,3 +87,14 @@ def reset_vm(vmid):
         return redirect(url_for('home'))
     else:
         return redirect(url_for('home'))
+
+
+# Admin Section
+
+# Users View
+
+@app.route('/admin/users')
+@login_required
+def admin_users():
+    users = User.query.order_by(User.id.desc())
+    return render_template('admin/users.html', title='Home', slug='Control VMs', users=users)
